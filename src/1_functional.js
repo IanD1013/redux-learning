25ac20982f8ddbffed36dce38a964268b7ee7680
IanD1013 <ian.tung1013@gmail.com>
2024-11-30 11:24:32 +1300

    Function composition

--- src/1_functional.js
+++ src/1_functional.js
@@ -18,3 +18,13 @@ function sayHello2() {
         return 'Hello, World!';
     }
 }
+
+/** Function Composition */
+let input = '   JavaScript   ';
+let output = '<div>' + input.trim() + '</div>';
+
+const trim = str => str.trim();
+const wrapInDiv = str => `<div>${str}</div>`;
+const toLowerCase = str => str.toLowerCase();
+
+const result = wrapInDiv(toLowerCase(trim(input)));
