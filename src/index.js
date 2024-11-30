a12273adcf4c23682e853d082721bded427dbe78
IanD1013 <ian.tung1013@gmail.com>
2024-11-30 18:03:36 +1300

    Dispatch actions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import store from './store';
+
+console.log(store)
+
+store.dispatch({
+    type: "bugAdded",
+    payload: {
+        description: "Bug1"
+    }
+})
+
+store.dispatch({
+    type: "bugRemoved",
+    payload: {
+        id: 1
+    }
+})
+
+console.log(store.getState())
