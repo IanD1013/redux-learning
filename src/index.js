fefd5e8f16e9eef1176af5e5d62eeb9364971073
IanD1013 <ian.tung1013@gmail.com>
2024-12-01 10:25:38 +1300

    Redux toolkit exercise - projects slice

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import configureStore from './store/configureStore';
 import * as actions from './store/bugs';
+import { projectAdded } from './store/projects';
 
 const store = configureStore();
+
+store.dispatch(projectAdded({ name: "Project 1" }));
 
 store.dispatch(actions.bugAdded({ description: "Bug 1" }));
 store.dispatch(actions.bugAdded({ description: "Bug 2" }));
