39290ec5ce20aaab8cca22961b664e7633a4e40a
IanD1013 <ian.tung1013@gmail.com>
2024-12-01 11:18:37 +1300

    Selectors

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -24,4 +24,5 @@ const slice = createSlice({
 export const { bugAdded, bugResolved } = slice.actions;
 export default slice.reducer;
 
-
+// Selector
+export const getUnresolvedBugs = state => state.entities.bugs.filter(bug => !bug.resolved);
