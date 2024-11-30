3a8d634ff32c2616f5411dd7cbfba453a1b90c92
IanD1013 <ian.tung1013@gmail.com>
2024-11-30 20:17:22 +1300

    Debug redux applications - redux devtools

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import store from "./customStore"
-import * as actions from "./actions"
-
-store.subscribe(() => {
-    console.log("Store changed!");
-})
+import store from './store';
+import * as actions from './actions';
 
 store.dispatch(actions.bugAdded("Bug 1"));
-
-console.log(store.getState());
+store.dispatch(actions.bugAdded("Bug 2"));
+store.dispatch(actions.bugAdded("Bug 3"));
+store.dispatch(actions.bugResolved(1));
