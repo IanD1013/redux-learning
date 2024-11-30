e47a89b6db08df8ce299889a28d81e9ccf157fab
IanD1013 <ian.tung1013@gmail.com>
2024-11-30 18:37:36 +1300

    Implement resolving a bug

--- src/actions.js
+++ src/actions.js
@@ -11,3 +11,8 @@ export const bugAdded = (description) => ({
   type: actions.BUG_ADDED,
   payload: { description },
 });
+
+export const bugResolved = (id) => ({
+  type: actions.BUG_RESOLVED,
+  payload: { id },
+});
