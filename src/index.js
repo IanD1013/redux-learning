718bc77b038ef7856ff424f05a7741e57da68c28
IanD1013 <ian.tung1013@gmail.com>
2024-12-01 13:51:56 +1300

    Dispatch functions - redux thunk

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-import configureStore from './store/configureStore';
-import { bugAdded, bugResolved, bugAssignedToUser, getUnresolvedBugs, getBugsByUser } from './store/bugs';
-import { projectAdded } from './store/projects';
-import { userAdded } from './store/users';
+import configureStore from "./store/configureStore";
 
 const store = configureStore();
 
-store.dispatch(userAdded({ name: "User 1" }));
-// store.dispatch(userAdded({ name: "User 2" }));
-
-// store.dispatch(projectAdded({ name: "Project 1" }));
-
-// store.dispatch(bugAdded({ description: "Bug 1" }));
-// store.dispatch(bugAdded({ description: "Bug 2" }));
-// store.dispatch(bugAdded({ description: "Bug 3" }));
-// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }))
-// store.dispatch(bugResolved({ id: 1 }));
-
-const unresolvedBugs = getUnresolvedBugs(store.getState())
-const bugs = getBugsByUser(1)(store.getState())
+store.dispatch((dispatch, getState) => {
+    dispatch({ type: 'bugsReceived', bugs: [1, 2, 3] });
+    console.log(getState());
+})
