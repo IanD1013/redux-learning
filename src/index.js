b87e8f1870da0fd79f85e4503ba284bce1ceb26d
IanD1013 <ian.tung1013@gmail.com>
2024-12-01 13:31:29 +1300

    Create middleware

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import { userAdded } from './store/users';
 const store = configureStore();
 
 store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
+// store.dispatch(userAdded({ name: "User 2" }));
 
-store.dispatch(projectAdded({ name: "Project 1" }));
+// store.dispatch(projectAdded({ name: "Project 1" }));
 
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }))
-store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugAdded({ description: "Bug 3" }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }))
+// store.dispatch(bugResolved({ id: 1 }));
 
 const unresolvedBugs = getUnresolvedBugs(store.getState())
 const bugs = getBugsByUser(1)(store.getState())
