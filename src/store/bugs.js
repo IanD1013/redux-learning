d335ce83d8cd69a67e4e6a652b1a97e35dae40e1
IanD1013 <ian.tung1013@gmail.com>
2024-12-07 21:32:12 +1300

    Exercise - assign a bug to a user

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -23,7 +23,7 @@ const slice = createSlice({
             bugs.loading = false;
         },
         bugAssignedToUser: (bugs, action) => {
-            const { bugId, userId } = action.payload;
+            const { id: bugId, userId } = action.payload;
             const index = bugs.list.findIndex(bug => bug.id === bugId);
             bugs.list[index].userId = userId;
         },
@@ -71,6 +71,13 @@ export const resolveBug = id => apiCallBegan({
     method: 'patch',
     data: { resolved: true },
     onSuccess: bugResolved.type
+})
+
+export const assignBugToUser = (bugId, userId) => apiCallBegan({
+    url: url + '/' + bugId,
+    method: 'patch',
+    data: { userId },
+    onSuccess: bugAssignedToUser.type
 })
 
 export const getUnresolvedBugs = createSelector(
